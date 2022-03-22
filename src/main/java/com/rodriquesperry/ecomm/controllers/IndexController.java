58bd3f72a82a1293b3f38830f4583d2a8a1ccf92
Rodriques <rodriques.perry@gmail.com>
2022-03-22 13:45:36 -0500

    populate products on index page

--- src/main/java/com/rodriquesperry/ecomm/controllers/IndexController.java
+++ src/main/java/com/rodriquesperry/ecomm/controllers/IndexController.java
@@ -1,13 +1,22 @@
 package com.rodriquesperry.ecomm.controllers;
 
+import com.rodriquesperry.ecomm.repositories.ProductRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    private final ProductRepository productDao;
+
+    public IndexController(ProductRepository productDao) {
+        this.productDao =productDao;
+    }
+
     @GetMapping("/")
-    public String showIndexPage() {
+    public String showIndexPage(Model model) {
+        model.addAttribute("products", productDao.findAll());
         return "index";
     }
 }
